6d51908527f445576f8877bbc6672346a57e65e9
DaebangStn <gunho1107@naver.com>
2021-08-28 13:48:32 +0900

    ...

--- app1/api_img/urls.py
+++ app1/api_img/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import FileView
+from . import views
 
 urlpatterns = [
-    path('upload/', FileView.as_view(), name='file-upload'),
+    path('upload/', views.FileView.as_view(), name='file-upload'),
+    path('open/', views.open, name='open'),
+    path('close/', views.close, name='close'),
+    path('state/', views.state, name='state'),
 ]
--- app1/api_img/views.py
+++ app1/api_img/views.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+from django.http.response import Http404
+from django.http import HttpResponse
+
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
@@ -8,6 +11,9 @@ from rest_framework import status
 from .serializers import FileSerializer
 from .classify import classify
 
+from .models import State
+
+THRESHOLD = 10
 
 class FileView(APIView):
   parser_classes = (MultiPartParser, FormParser)
@@ -23,8 +29,46 @@ class FileView(APIView):
       imagepath = os.path.join(MEDIA_ROOT, str(t.file))
 
       result = classify(imagepath)
+
+      state = State()
+      if result > THRESHOLD:
+        State.motor = True  
+        print('motor true')
+      else:
+        State.motor = False
+        print('motor false')
+
+      state.save()             
       print('Result:\t', result)
 
       return Response(file_serializer.data, status=status.HTTP_201_CREATED)
     else:
       return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def open(request):
+    state = State()
+    state.motor = True
+    state.save()
+    return HttpResponse("submitted")
+
+
+def close(request):
+    state = State()
+    state.motor = False
+    state.save()
+    return HttpResponse("submitted")
+
+
+def state(request):
+    try:
+        state_recent = State.objects.last()
+    except:
+        raise Http404("no item")
+
+    print('state: ' + str(state_recent))
+
+    if str(state_recent) == 'True':
+        return HttpResponse("True")          
+    else:
+#        return HttpResponse("False")          
+        raise Http404("no item")
